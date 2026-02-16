7cc8f83f711d875f659d015a13f63e4fcbcae2c8
AbdullahAlHadabi <Abdullah.Juma280@hotmail.com>
2026-02-16 15:11:10 +0400

    "new update1"

--- Checkpointcombo/src/Checkpointcombo.java
+++ Checkpointcombo/src/Checkpointcombo.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
+
 public class Checkpointcombo {
-    public static void main(String [] agrs){
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("chooes your ticket type");
-
         String[] tickets = {"Cinema Ticket", "Bus Ticket", "Concert Ticket"};
         double[] prices = {5.0, 2.5, 15.0};
-
 
         System.out.println("===== Ticket Menu =====");
         for (int i = 0; i < tickets.length; i++) {
@@ -18,26 +16,26 @@ public class Checkpointcombo {
         System.out.print("Choose a ticket (1-3): ");
         int choice = sc.nextInt();
 
-        if(choice >= 1 && choice <= tickets.length){
+        if (choice >= 1 && choice <= tickets.length) {
 
             System.out.println("You selected: " + tickets[choice - 1]);
             System.out.println("Price: $" + prices[choice - 1]);
 
+            System.out.print("\nChoose new ticket to update (1-3): ");
+            int newChoice = sc.nextInt();
 
+            if (newChoice >= 1 && newChoice <= tickets.length) {
+                System.out.println("\nUpdated Ticket:");
+                System.out.println("Ticket: " + tickets[newChoice - 1]);
+                System.out.println("Price: $" + prices[newChoice - 1]);
+            } else {
+                System.out.println("Invalid update choice!");
+            }
 
-        }
-        else {
+        } else {
             System.out.println("Invalid choice!");
         }
 
         sc.close();
-
-
-
-
-
-
-
-
     }
 }
